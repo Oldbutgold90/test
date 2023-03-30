422969476cac91d26dc871f729129441d3eb3af2
Oldbutgold90 <expert023@ukr.net>
2023-03-30 20:47:06 +0300

    123

---
+++ test.py
@@ -0,0 +1,9 @@
+n = int(input('lvls - '))
+number = 1
+for row in range(1, n+1):
+    print('\t' * (n - row), end = '')
+    for col in range(row):
+        print(number, end = '')
+        number += 2
+        print('\t' * 2, end = '')
+    print()
